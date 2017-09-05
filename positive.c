b31824c86631f90c6ef2254705ae15e2483d9955
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2017-09-05 16:36:46 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the value");
+scanf("%d",&i);
+if(num > 0);
+{
+printf("enter the positive values");
+}
+if(num < 0)
+{
+printf("enter the negative values");
+}
+if(num ==0)
+{ 
+print("enter the zeeo values");
+}
+return 0;
+}
