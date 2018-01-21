213c9c428677d65e20f13cd66fd5298da298cf22
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-01-21 10:59:37 +0530

    Update minimum.c

--- minimum.c
+++ minimum.c
@@ -10,23 +10,21 @@ for (c = 0; c < size; c++)
 scanf("%d", &array[c]):
 location = find_minimum(array, size);
 minimum  = array[location];
- 
-  printf("Minimum element location = %d and value = %d.\n", location + 1, minimum);
-  return 0;
+printf("Minimum element location = %d and value = %d.\n", location + 1, minimum);
+return 0;
 }
- 
-int find_minimum(int a[], int n) {
-  int c, min, index;
- 
-  min = a[0];
-  index = 0;
- 
-  for (c = 1; c < n; c++) {
-    if (a[c] < min) {
-       index = c;
-       min = a[c];
-    }
-  }
- 
-  return index;
+int find_minimum(int a[], int n) 
+{
+int c, min, index;
+min = a[0];
+index = 0;
+for (c = 1; c < n; c++) 
+{
+if (a[c] < min) 
+{
+index = c;
+min = a[c];
+}
+}
+return index;
 }
