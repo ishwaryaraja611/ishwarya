13636e5286ed4c84acd17665752963246dbe8538
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-01-23 15:47:32 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+int hour,min=120;
+printf("enter the time and minute"):
+scanf("%d,%d,&time &minute");
+{
+while(min>60)
+{
+hour++;
+min=min-60;
+printf("time and minute is /n");
+}
+}
