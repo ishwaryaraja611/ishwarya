e0fa81a6d90697478c1487b7cf9a1374ef55b9ae
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-01-25 16:55:34 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,k;
+char a[100];
+printf("number of times to be printed:");
+scanf("%d",&k);
+printf("enter the char\n");
+scanf("%s",&a);
+for(i=0;i<k;i++)
+{
+printf("%s\n",a);
+}
+getch();
+}
