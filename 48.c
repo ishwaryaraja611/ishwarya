90b418c9ed2daa9c80f00ba9d8e656cdf2d1bb6a
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-02-02 09:20:57 +0000

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a=10,b=4,c=3,d=7,e=8,sum;
+sum=a+b+c+d+e;
+scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+printf("print the sum of average number \n",sum);
+if(sum%5)
+{
+printf("print the average number");
+}
+}
+
+
