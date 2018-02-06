a438d73c9628ae625cc35223b558a7ae96fe17f8
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-02-06 08:50:41 +0000

    Update 52.c

--- 52.c
+++ 52.c
@@ -7,37 +7,37 @@ scanf("%d",&n);
 switch(n)
 {
 case 0: 
-printf("ZERO");
+printf("ZERO \n");
 break;
 case 1:
-printf("ONE"); 
+printf("ONE \n"); 
 break;
 case 2: 
-printf("TWO");
+printf("TWO \n");
 break;
 case 3:
-printf("THREE");
+printf("THREE \n");
 break;
 case 4:
-printf("FOUR");
+printf("FOUR \n");
 break;
 case 5:
-printf("FIVE");
+printf("FIVE \n");
 break;
 case 6:
-printf("SIX");
+printf("SIX \n");
 break;
 case 7:
-printf("SEVEN");
+printf("SEVEN \n");
 break;
 case 8:
-printf("EIGHT");
+printf("EIGHT \n");
 break;
 case 9:
-printf("NINE");
+printf("NINE \n");
 break;
 default:
-printf(" MORE THAN 1 DIGIT");
+printf(" MORE THAN 1 DIGIT \n");
 break;
 return 0;
 }
