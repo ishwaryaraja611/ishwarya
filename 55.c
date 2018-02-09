0fdf3e927ac527ff081753046b722142cb146710
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-02-09 08:34:33 +0000

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int n,pro;
+printf("enter the number1:");
+scanf("%d",&n);
+pro=n;
+if(pro%2==0)
+{
+printf("\n even");
+}
+else
+{
+printf("\n odd");
+}
+}
+ 
