1c57aef857ecd91b01564787c6b2228a780f9daf
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-03-02 16:45:10 +0530

    Create 111.c

---
+++ 111.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+  int num,count=0;
+  printf("enter the number");
+  scanf("%d",&num);
+  while(num!=0)
+  {
+    num=num/10;
+    count++;
+  }
+  printf("%d\n",count);
+  
+}
