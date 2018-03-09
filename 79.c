0684c8538ebc696dce9c7abca2d5a40850ad0f54
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-03-09 16:29:26 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+int main(void)
+{
+  int a,b;
+  printf("enter the number a and b ");
+  scanf("%d %d",&a,&b);
+ if(a==b)
+ {
+   printf("yes");
+  }
+  else
+  {
+  printf("no");
+  }
+  return 0;
+}
