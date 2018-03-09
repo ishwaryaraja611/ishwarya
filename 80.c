2d30392add0a0b2fe8f8cc2b2a6f77982616228a
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-03-09 16:41:19 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+int main(void)
+{
+  int a,b;
+  printf("enter the number a");
+  scanf("%d",&a);
+ while(a>0)
+ {
+   b=a%10;
+   if(b%2!=0)
+   {
+     printf("%d",b);
+   }
+   a=a/10;
+ }
+  return 0;
+}
