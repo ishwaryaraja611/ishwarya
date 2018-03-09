1b17559f9aff348ee6deb29f34d3f7740bb0126a
ishwaryaraja611 <31504463+ishwaryaraja611@users.noreply.github.com>
2018-03-09 16:15:31 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+int main(void)
+{
+  int num,i,c=0;
+  printf("enter the number ");
+  scanf("%d",&num);
+  for(i=1;i<=1;i++)
+  {
+    if((c%i==0))
+    {
+      c++;
+    }
+      }
+  if((c==2))
+  {
+    printf("No");
+  }
+  else
+  {
+    printf("yes");
+  }
+  return 0;
+}
